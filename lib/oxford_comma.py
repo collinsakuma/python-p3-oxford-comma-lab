42361fa4ca14e75b5db7a633f2c92ef2f4fe0a9b
Collin Sakuma <collinmsakuma@gmail.com>
2023-03-14 15:44:23 -0700

    complete

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if (len(items) == 2):
+        return " and ".join(items)
+    elif (len(items) >= 3):
+        last_item = items.pop()
+        return ", ".join(items) + f", and {last_item}"
+    else:
+        return "".join(items)
+
+print(oxford_comma(["fiddleheads", "okra", "kohlrabi"]))
